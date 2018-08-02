cb085fd293a020b4e07e4c11f14faf8eaee75ab1
dhirajataws <dhirajataws@gmail.com>
2018-08-03 06:14:56 +1000

    Exception handliing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
 var fs = require('fs');
 
-exports.reducer = reducer = function (accumulator, currentValue, index) {
-
+exports.reducer = reducer = function (accumulator, currentValue) {
   const price = parseFloat(currentValue.price);
   accumulator.sum = accumulator.sum + price;
   accumulator.max = accumulator.max > price ? accumulator.max : price;
@@ -28,35 +27,34 @@ exports.fetchBids = fetchBids = function () {
   return axios({ // return axios promise
     url: 'https://api.bitfinex.com/v1/book/BTCUSD?limit_bids=1000?limit_asks=1000',
     method: 'get',
-  }).then(function (response) {
-    if (response.status !== 200) {
-      throw new Error('nok') // not ok
-    }
-    if (response.data && response.data.bids !== undefined) {// obj.create
-      const bids = response.data.bids;
-      bidSummary.min = bidSummary.max = parseFloat(bids[0].price);
-      bidSummary = bids.reduce(reducer, bidSummary);
-      bidSummary.average = bidSummary.sum / bids.length;
-      bidSummary.median = calculateMedian(bids);
-      delete bidSummary['sum']
-
-      return bidSummary;
-    } else {
-      throw new Error('no data found')
-    }
-  }).catch(function (e) {
-    console.log(e);
-    throw (e)
   })
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw new Error('nok') // not ok
+      }
+      if (response.data && response.data.bids !== undefined) {// obj.create
+        const bids = response.data.bids;
+        bidSummary.min = bidSummary.max = parseFloat(bids[0].price);
+        bidSummary = bids.reduce(reducer, bidSummary);
+        bidSummary.average = bidSummary.sum / bids.length;
+        bidSummary.median = calculateMedian(bids);
+        delete bidSummary['sum']
+        return bidSummary;
+      } else {
+        throw new Error('no data found')
+      }
+    })
+    .catch(function (e) {
+      console.log(e);
+      throw (e)
+    })
 }
 exports.readData = readData = function () {
   return new Promise(function (resolve, reject) {
     fs.readFile('data.json', 'utf8', function readfile(err, data) {
       if (err) {
-        console.log(err);
-        reject('fail');
+        reject(err);
       } else {
-        console.log(data);
         obj = JSON.parse(data); //now it an object
         resolve(obj);
       }
@@ -64,22 +62,51 @@ exports.readData = readData = function () {
   })
 }
 
-exports.writeData = writeData = function () {
+exports.writeData = writeData = function (json) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile('data.json', json, 'utf8', function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('write to data.json done');
+      }
+    })
+  })
+}
+
+exports.getBids = getBids = function () {
   var obj = {
     table: []
   };
-  Promise.all([fetchBids(), readData()]).then(function (values) {
-    obj.table = values[1].table;
-    obj.table.push(values[0]);
-    var json = JSON.stringify(obj); //convert it back to json
-    fs.writeFile('data.json', json, 'utf8', function (err, data) {
-    }); // write it back
-
-  }).catch(function (err) {
-      console.log("hello" + err);
+  return Promise.all([fetchBids(), readData()])
+    .then(function (values) {
+      obj.table = values[1].table;
+      obj.table.push(values[0]);
+      var json = JSON.stringify(obj); //convert it back to json
+      return json;
+    })
+    .catch(function (e) {
+      throw(e)
     })
 }
 
 exports.writeBidsSummary = function () {
-  setInterval(writeData, 10000);
+  var intervalId = setInterval(function () {
+    getBids().then(function (json) {
+      writeData(json)
+        .then(function (success) {
+          console.log(success); // Not sure if it is required
+        })
+        .catch(function (e) {
+          console.log(e);
+          console.log('process going to stop')
+          clearInterval(intervalId)
+        })
+    })
+      .catch(function(e){
+        console.log(e);
+        console.log('process going to stop')
+        clearInterval(intervalId)
+      });
+  }, 10000);
 }
